bade12ee6f41892c24a684ba90b91290aa1e3325
Ysaxyor <aurel.bournonville@icloud.com>
2020-03-17 14:25:01 +0100

    organisation

--- retard/taquin/main.java
+++ retard/taquin/main.java
@@ -1,8 +1,12 @@
 package taquin;
 import taquin.model.*;
+import taquin.view.*;
+import taquin.controller.*;
 
 public class main{
 	public static void main(String [] args){
+		new GUI();
+
 		Grille grille = new Grille(3,3);
 		grille.melanger(10);
 		grille.afficher();
